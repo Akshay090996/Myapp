d26b40922bf6a3dab937ba66d41f6eeb8207152f
Akshay090996 <gowda.akshay27@gmail.com>
2022-02-12 18:16:07 +0000

    today commitment

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,7 +1,19 @@
+//@desc     register a user
+//@route    /api/users
+//@access   public
 const registerUser = (req, res) => {
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error("Please add all the fields")
+  }
   res.send("Register Route")
 }
 
+//@desc     login a user
+//@route    /api/users/login
+//@access   public
 const loginUser = (req, res) => {
   res.send("Login Route")
 }

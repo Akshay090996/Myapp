5e38469e9c274f947cdbc64f0af8b3ef7450ac13
Akshay090996 <gowda.akshay27@gmail.com>
2022-02-13 16:10:09 +0000

    13/2/22

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -64,7 +64,7 @@ const loginUser = asyncHandler(async (req, res) => {
     })
   } else {
     res.status(401)
-    throw new Error("Invaild credential")
+    throw new Error("Invalid credential")
   }
 })
 
